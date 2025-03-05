73e683dc62e438f3581ba3b2a2345af8f82f00eb
Diamond <dbrown@pursuit.org>
2025-03-05 00:04:54 -0500

    completed lesson 18-27 warmup questions

--- lesson22-sliding-window/warmup.js
+++ lesson22-sliding-window/warmup.js
@@ -1,4 +1,27 @@
 // 1. Given an array of integers return the largest number
-
+function findLargestNumber(arr) {
+    return Math.max(...arr);
+  }
+  
+//   console.log(findLargestNumber([1, 5, 3, 9, 2]));  // Output: 9
+//   console.log(findLargestNumber([-1, -5, -3, -9, -2]));  // Output: -1
+  
 // 2. Given an array of integers return the largest sum for two consecutive numbers
-// input: [20,17,25] output: 42
+
+function largestConsecutiveSum(arr) {
+    let left = 0;
+    let right = 1;
+    let maxSum = -Infinity;
+  
+    while (right < arr.length) {
+      const sum = arr[left] + arr[right];
+      maxSum = Math.max(maxSum, sum);
+      
+      left++;
+      right++;
+    }
+  
+    return maxSum;
+  }
+  
+//   console.log(largestConsecutiveSum([20, 17, 25]));  // Output: 42
